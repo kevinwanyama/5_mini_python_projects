6091b5e29bdde6c4c191c1ec599987c6fe8970c5
kevinwanyama <kvnwanyama@gmail.com>
2022-03-24 17:02:56 +0300

    The question game

--- game1.py
+++ game1.py
@@ -1,6 +1,35 @@
+from itertools import count
+
+
 print ("WELCOME TO MY GAME:")
 playing = input ("Do you want play?yes/no? ")
-if playing == "no":
+if playing.lower() != "yes":
     print("OOPs to bad for you")
     quit()
 print("yeeeeeh Let,s play: )")
+
+count=0
+answer = input('how many sides does a square have? ')
+if answer.lower() == "4":
+    print("correct ans")
+    count+=1
+else:
+    print("not correct")
+
+answer = input('how many sides does a Triangle have? ')
+if answer.lower() == "3":
+    print("correct ans")
+    count+=1
+else:
+    print("not correct")
+answer = input('how many sides does a Pentagon have? ')
+if answer.lower() == "5":
+    print("correct ans")
+    count+=1
+else:
+    print("not correct")  
+print("your score is: "+ str((count/3)*100) + "%")  
+if count > 2:
+    print('Above avarage')
+else:
+    print("below avarage")
