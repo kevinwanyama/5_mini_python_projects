f3e26b2428e2c0f04d8dcb4ec87dccecb60b0870
kevinwanyama <kvnwanyama@gmail.com>
2022-04-05 11:48:11 +0300

    password manager

---
+++ password_manager.py
@@ -0,0 +1,50 @@
+from cryptography.fernet import Fernet
+
+
+'''def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt','r')as f:
+        print("F here", f)
+        for line in f.readlines():
+            data=line.rstrip()
+            user , passw = data.split("|")
+            print("User: ", user,"Password: ",fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name =input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt','a')as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+while True:
+
+    mode =  input("add or view passwore. add/view?? q to quit: ").lower()
+    if mode == "q":
+        break
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
+
+
